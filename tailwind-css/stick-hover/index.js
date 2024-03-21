df070f3560286c0db6d69c8cb71d3f1f4926818c
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-21 13:21:25 +0530

    Stick Hover using tailwind css and javascript

---
+++ tailwind-css/stick-hover/index.js
@@ -0,0 +1,17 @@
+function projectData() {
+    return {
+        projects: [
+            {
+                id: 1,
+                title: "Project 1",
+                category: "Design",
+                url: "#",
+                users: [
+                    {id: 1, name: "User 1", avatar: "path/to/avatar1.png"},
+                    {id: 2, name: "User 2", avatar: "path/to/avatar2.png"},
+                ]
+            },
+            // Add more projects as needed
+        ]
+    };
+}
